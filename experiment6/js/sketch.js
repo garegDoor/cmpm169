e140f1992f6e9e58cb47ed4fdd498f3929dd5a71
Gareg <reg_ga@farmorereal.com>
2025-02-21 00:37:20 -0800

    Updated note at the top of sketch.js

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,6 +1,6 @@
 // sketch.js - purpose and description here
 // Author: Gabe Ahrens
-// Date: 1/27/2025
+// Date: 2/20/2025
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
